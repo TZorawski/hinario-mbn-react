8110d9dadd05ffc861dc2cb33217f2b80aa39386
Thais Zorawski <thaiszorawski@gmail.com>
2025-07-09 14:45:32 -0300

    global: create project with navigation

---
+++ src/pages/Hino/index.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Hino(){
+  return(
+    <View style={styles.container}>
+      <Text>Pagina sobre</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
